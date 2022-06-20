19381982fac8c32be3a60c5cc9fee50c16305691
Przemek Badaruk <przemekkth@outlook.com>
2022-06-20 14:03:00 +0200

    Better collision

--- enemy.h
+++ enemy.h
@@ -6,13 +6,15 @@
 
 struct Enemy
 {
-    int x,y,dx,dy;
+    int x,y,dx,dy, pixmapWidth, pixmapHeight;
 
       Enemy()
        {
         x=y=300;
         dx=4-rand()%8;
         dy=4-rand()%8;
+        pixmapWidth = 40;
+        pixmapHeight = 40;
        }
 
       void move()
@@ -23,8 +25,19 @@ struct Enemy
             dx=-dx;
             x+=dx;
         }
+        if (Game::grid[y/Game::TILE_SIZE][(x+pixmapWidth/2)/Game::TILE_SIZE]==1)
+        {
+            dx=-dx;
+            x+=dx;
+        }
+
         y+=dy;
         if (Game::grid[y/Game::TILE_SIZE][x/Game::TILE_SIZE]==1)
+        {
+            dy=-dy;
+            y+=dy;
+        }
+        if (Game::grid[(y+pixmapHeight/2)/Game::TILE_SIZE][x/Game::TILE_SIZE]==1)
         {
             dy=-dy;
             y+=dy;
